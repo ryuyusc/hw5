279c76e7ca83d499ac409010f66c33e466c558b0
Youngbin Ryu <ryuy@usc.edu>
2023-04-12 21:29:29 +0000

    finished

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,7 +21,13 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool fill(const AvailabilityMatrix& avail,
+              const size_t dailyNeed,
+              const size_t maxShifts,
+              DailySchedule& sched,
+              vector<Worker_T>& numShifts,
+              const size_t day,
+              const size_t number);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -38,8 +44,61 @@ bool schedule(
     sched.clear();
     // Add your code below
 
+    //constructs the schedule 2d array
+    for (size_t i = 0; i < avail.size(); i++) {
+      vector<Worker_T> toAdd(dailyNeed);
+      sched.push_back(toAdd);
+    }
 
 
+    //this will keep track of the number of shifts
+    //each worker has done so far
+    vector<Worker_T> numShifts(avail[0].size());
+    for (size_t i = 0; i < avail[0].size(); i++) {
+      numShifts[i] = 0;
+    }
 
+  return fill(avail, dailyNeed, maxShifts, sched, numShifts, 0, 0);
 }
+
+bool fill(const AvailabilityMatrix& avail,
+          const size_t dailyNeed,
+          const size_t maxShifts,
+          DailySchedule& sched,
+          vector<Worker_T>& numShifts,
+          const size_t day,
+          const size_t number) {
+    //true if everything is filled up and the day is out of bounds
+    if (day == avail.size()) {
+      return true;
+    }
+
+    //iterates through availability for that day
+    //i represents the worker ID
+    for (size_t i = 0; i < avail[day].size(); i++) {
+      if (avail[day][i] && numShifts[i] < maxShifts) {
+        sched[day][number] = i;
+        numShifts[i] = numShifts[i] + 1;
+        bool status = false;
+
+        if (number == dailyNeed - 1) {
+          status = fill(avail, dailyNeed, maxShifts, sched, numShifts, day + 1, 0);
+        }
+        else {
+          status = fill(avail, dailyNeed, maxShifts, sched, numShifts, day, number + 1);
+        }
+
+        if (status) {
+          return true;
+        }
+
+        sched[day][number] = -1;
+        numShifts[i] = numShifts[i] - 1;
+      }
+    }
+
+    return false;
+}
+
+
 
--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,10 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void fillset(std::set<std::string>& words, 
+            const std::string& in, 
+            const std::string& floating,
+            const std::set<std::string>& dict);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +25,50 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
-
+    set<string> ans;
+    fillset(ans, in, floating, dict);
+    return ans;
 }
 
 // Define any helper functions here
+void fillset(std::set<std::string>& words, 
+            const std::string& in, 
+            const std::string& floating,
+            const std::set<std::string>& dict) {
+    //case in which a word is completed and 
+    //added only if it exists in dict
+    if (in.find('-') == string::npos) {
+        if (dict.find(in) != dict.end()) {
+            words.insert(in);
+        }
+    }
+    else {
+        size_t index = in.find('-');
+        //recurses through floating letters first
+        for (size_t i = 0; i < floating.size(); i++) {
+            string newString(in);
+            string s(1, floating[i]);
+            newString.replace(index, 1, s);
+
+            string newFloating(floating);
+            newFloating.erase(i, 1);
+            fillset(words, newString, newFloating, dict);
+        }
+
+        //recurses through the letters of the alphabet
+        int numLetters = 0;
+        for (size_t i = 0; i < in.size(); i++) {
+            if (in[i] != '-') {
+                numLetters++;
+            }
+        }
+        if (numLetters + floating.size() < in.size()) {
+            for (size_t i = 0; i < 26; i++) {
+                string temp(1, 'a' + i);
+                string newString(in);
+                newString.replace(index, 1, temp);
+                fillset(words, newString, floating, dict);
+            }
+        }
+    }
+}
